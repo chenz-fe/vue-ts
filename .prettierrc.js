738393e628267d554e32151300f833eab1a705cc
zengchen <zengchen98@163.com>
2020-04-12 07:38:49 +0800

    add ts

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,7 @@ module.exports = {
   tabWidth: 2,
   useTabs: false,
   singleQuote: true,
-  semi: true,
+  semi: false,
   trailingComma: 'none',
   bracketSpacing: true,
   jsxBracketSameLine: true
--- src/test.ts
+++ src/test.ts
@@ -1,35 +1,83 @@
 // 类型注解
-let title: string;
+let title: string
 // title = 2
-title = 'xbl';
+title = 'xbl'
 
-let list: string[];
-list = ['xbl'];
+let list: string[]
+list = ['xbl']
 // list = [1, 2]
 
-let arr: any[];
-arr = ['xbl', 1, 2];
+let arr: any[]
+arr = ['xbl', 1, 2]
 
 function greeting1(person: string): string {
-  return 'Hello ' + person;
+  return 'Hello ' + person
 }
 
 function info1(): void {
-  console.log('This is info msg');
+  console.log('This is info msg')
 }
 
 function greeting(person: string, msg?: string) {
-  return 'Hello ' + person + ' ' + msg;
+  return 'Hello ' + person + ' ' + msg
 }
-console.log(greeting('xbl', 'nihaowa'));
+console.log(greeting('xbl', 'nihaowa'))
 
 // 函数重载
-function info(a: { name: string }): string;
-function info(a: string): { name: string };
+function info(a: { name: string }): string
+function info(a: string): { name: string }
 function info(a: { name: string } | string): string | { name: string } {
   if (typeof a === 'object') {
-    return a.name;
+    return a.name
   } else {
-    return { name: a };
+    return { name: a }
   }
+}
+
+class MyComp {
+  private _foo: string // 私有属性，不能在类的外部访问
+  protected bar: string // 保护属性，可以在子类中访问
+  // 构造函数参数加修饰符，能够定义为成员属性
+  constructor(public tua = 'tua', fooVal: string, barVal: string) {
+    this._foo = fooVal
+    this.bar = barVal
+  }
+  // 方法也有修饰符
+  private someMethod() {
+    return ''
+  }
+  // 存取器：属性方式访问，可添加额外逻辑，控制读写性
+  get foo() {
+    return this._foo
+  }
+  set foo(val) {
+    this._foo = val
+  }
+}
+
+// 接口
+interface Person {
+  firstName: string
+  lastName: string
+}
+
+function greeting2(person: Person) {
+  return 'Hello, ' + person.firstName + ' ' + person.lastName
+}
+
+const xbl = { firstName: 'x', lastName: 'bl' }
+console.log(greeting2(xbl))
+
+// 泛型
+interface Result<T> {
+  ok: 0 | 1
+  data: T
+}
+
+function getData<T>(): Result<T> {
+  const data: any = [
+    { id: 1, name: '类型注解' },
+    { id: 2, name: '编译型语言' }
+  ]
+  return { ok: 0, data }
 }
